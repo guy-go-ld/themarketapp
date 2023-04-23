b47da726f5dbb2fc74be70a248762347eab2463e
Yuval Lavie <yuval5000l@gmail.com>
2023-04-23 19:19:42 +0300

    trying to transfer data to profile page

--- src/routes/profile_page/ProfilePage.js
+++ src/routes/profile_page/ProfilePage.js
@@ -1,6 +1,13 @@
 import {Component} from "react";
+import data from "../../databases/ListData.json";
 
 class ProfilePage extends Component{
+    constructor(id) {
+        super();
+        this.state={
+            profileID:id
+        };
+    }
     render(){
         return(
             <div>
