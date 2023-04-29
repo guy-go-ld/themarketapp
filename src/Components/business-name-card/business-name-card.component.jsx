2e56113e767c24ff58b24b8056f6316592bbe227
Yuval Lavie <yuval5000l@gmail.com>
2023-04-29 18:26:09 +0300

    transfering data to businessPage

--- src/Components/business-name-card/business-name-card.component.jsx
+++ src/Components/business-name-card/business-name-card.component.jsx
@@ -7,9 +7,11 @@ class BusinessNameCard extends Component{
         const {id,name,title}=this.props.business
         return (
             <div className="card-contatiner" key={id}>
+                <Link to={'../BusinessPageComponent'} state={{ from: id}}>
                 <img alt={`business ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
                 <h2>{name}</h2>
                 <p>{title}</p>
+                </Link>
             </div>
         )
     }
--- src/routes/business_page/BusinessPage.component.jsx
+++ src/routes/business_page/BusinessPage.component.jsx
@@ -1,13 +1,36 @@
 import {Component} from "react";
+import {useLocation} from 'react-router-dom'
+import data from "../../databases/ListData.json"
 
-class BusinessPageComponent extends Component{
-    render(){
-        return(
-            <div>
-                <h1 >My Business Page</h1>
-            </div>
-        );
-    }
+
+export default function BusinessPageComponent()
+{
+    const location = useLocation()
+    const check_null = location.state === null;
+    let {from} = (check_null === true) ? 0 : location.state;
+
+    const data_on_person = data.filter((business) => business.id === from)[0]; // TODO better this stuff
+
+    return (
+        <div>
+            <h1>My Business Page</h1>
+            {(check_null === true) ?
+                <h2> Error - business page doesn't exists</h2>
+            :
+                <h2> {data_on_person.name} </h2>
+            }
+
+        </div>
+    );
 }
-
-export default BusinessPageComponent;
+// class BusinessPageComponent extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <h1 >My Business Page</h1>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default BusinessPageComponent;
