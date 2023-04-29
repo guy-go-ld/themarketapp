ebe0193152b2325e528fffce07c9d7d151db7dd7
Yuval Lavie <yuval5000l@gmail.com>
2023-04-29 22:12:51 +0300

    working on BusinessPage.component.jsx

--- src/routes/business_page/BusinessPage.component.jsx
+++ src/routes/business_page/BusinessPage.component.jsx
@@ -1,11 +1,19 @@
-import {Component} from "react";
 import {useLocation} from 'react-router-dom'
 import data from "../../databases/ListData.json"
-import {deepOrange} from "@mui/material/colors";
-import {Avatar, Stack} from "@mui/material";
+import {Avatar, Collapse, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Stack} from "@mui/material";
+import DialogBoxContact from "../../Components/dialog-box-contact/dialog_box_contact";
+import LastActivitiesFriendsDialog from "../../Components/dialog-box-basic/dialog_box_basic";
+import {ExpandLess, ExpandMore, StarBorder} from "@mui/icons-material";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import {useState} from "react";
 
-function showPerson(data_on_person)
+function ShowPerson(data_on_person)
 {
+    const [open, setOpen] = useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
     return(
         <div>
             <h1>{data_on_person.name}</h1>
@@ -15,6 +23,72 @@ function showPerson(data_on_person)
                         sx={{ width: 90, height: 90 }}/>
             </Stack>
             <h3>{data_on_person.title}</h3>
+            <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+                <DialogBoxContact props = {0}></DialogBoxContact>
+                <DialogBoxContact props = {1}></DialogBoxContact>
+                <DialogBoxContact props = {2}></DialogBoxContact>
+            </Stack>
+            <p></p>
+            Friends that used this business
+            <p></p>
+            <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+                <LastActivitiesFriendsDialog props={1}/>
+                <LastActivitiesFriendsDialog props={2}/>
+                <LastActivitiesFriendsDialog props={3}/>
+                <LastActivitiesFriendsDialog props={4}/>
+                <LastActivitiesFriendsDialog props={5}/>
+            </Stack>
+
+            <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+
+                <List
+                    sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                    component="nav"
+                    aria-labelledby="nested-list-subheader"
+                    subheader={
+                        <ListSubheader component="div" id="nested-list-subheader">
+                            Time Table Items
+                        </ListSubheader>
+                    }
+                >
+                    <ListItemButton onClick={handleClick}>
+                        <ListItemIcon>
+                            <AccessTimeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="TimeTable" />
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemButton>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <StarBorder />
+                                </ListItemIcon>
+                                <ListItemText primary="Today" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <StarBorder />
+                                </ListItemIcon>
+                                <ListItemText primary="Tomorrow" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <StarBorder />
+                                </ListItemIcon>
+                                <ListItemText primary="Yesterday" />
+                            </ListItemButton>
+                            <ListItemButton sx={{ pl: 4 }}>
+                                <ListItemIcon>
+                                    <StarBorder />
+                                </ListItemIcon>
+                                <ListItemText primary="Next year" />
+                            </ListItemButton>
+                        </List>
+                    </Collapse>
+                </List>
+            </Stack>
+            HERE
         </div>
     );
 }
@@ -31,7 +105,7 @@ export default function BusinessPageComponent()
             {(check_null === true) ?
                 (<div><h2> Error - business page doesn't exists</h2></div>)
             :
-                (<div>{showPerson(data_on_person)}</div>)
+                (<div>{ShowPerson(data_on_person)}</div>)
             }
 
         </div>
