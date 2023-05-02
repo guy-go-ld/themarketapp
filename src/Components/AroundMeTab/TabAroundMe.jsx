e215f4ae4e791b40d0150cd5f29902eb1589efc9
Hila <hilap97@gmail.com>
2023-05-02 11:10:48 +0300

    add tab to the around me page, one tab for map, second tab for list of around me activity (business cards). also, I wrote few TODOs related to my changes :)

---
+++ src/Components/AroundMeTab/TabAroundMe.jsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+// import SwappableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import ResizableMap from '../updatable-map/ResizableMap';
+import BusinessNameCard from "../business-name-card/business-name-card.component";
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
+export default function FullWidthTabs() {
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index) => {
+        setValue(index);
+    };
+
+    return (
+        <Box sx={{ bgcolor: 'background.paper', width: '100%' }}>
+            <AppBar position="static">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="secondary"
+                    textColor="inherit"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Map" {...a11yProps(0)} />
+                    <Tab label="List" {...a11yProps(1)} />
+                    {/*<Tab label="Item Three" {...a11yProps(2)} />*/}
+                </Tabs>
+            </AppBar>
+            {/*<SwappableViews*/}
+            {/*    axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}*/}
+            {/*    index={value}*/}
+            {/*    onChangeIndex={handleChangeIndex}*/}
+            {/*>*/}
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    <ResizableMap/>
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    {/*TODO: 1. create json file that connects to my friends/ community last activity shows the around me activity on map and as a cards list*/}
+                    <BusinessNameCard business={{"id": 1, "name": "Ofra's lak-gel", "title": "lak gel"}}/>
+                </TabPanel>
+                {/*<TabPanel value={value} index={2} dir={theme.direction}>*/}
+                {/*    Item Three*/}
+                {/*</TabPanel>*/}
+            {/*</SwappableViews>*/}
+        </Box>
+    );
+}
--- src/routes/Around Me/AroundMe.component.jsx
+++ src/routes/Around Me/AroundMe.component.jsx
@@ -1,17 +1,30 @@
 import './AroundMe.styles.css';
 import {Component} from "react";
 import ResizableMap from "../../Components/updatable-map/ResizableMap"
+import FullWidthTabs from "../../Components/AroundMeTab/TabAroundMe"
+import Box from '@mui/material/Box';
+// import { palette } from '@mui/system';
+
+
 
 class AroundMe extends Component{
 render() {
     return(
         <div>
-            <h1>
-                Around Me
-            </h1>
-            <ResizableMap/>
+            <Box sx={{ bgcolor: 'primary.main', color: 'primary.contrastText',p: 2, width: '100%' }}>
+                {/*TODO: 1. apply design like the tab bar. 2. make the h1 tag centered.*/}
+                <h1 style={{justifyContent: 'center', margin: 'auto'}}>
+                    Around Me
+                </h1>
+            </Box>
+            <FullWidthTabs/>
+            {/*<ResizableMap/>*/}
+
         </div>
     )
 }
 }
+// TODO:
+//  1. need to fix the bottom bar in this page- it is not in proportion.
+//  2. the bottom bar is also not fixed on the bottom- we have to scroll in order to see it.
 export default AroundMe
