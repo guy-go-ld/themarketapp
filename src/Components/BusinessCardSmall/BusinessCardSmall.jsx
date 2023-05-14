c469318dc0aa80e201efe6498b12ba33f904f29b
Yuval Lavie <yuval5000l@gmail.com>
2023-05-14 17:41:36 +0300

    Removing warnings

--- src/Components/BusinessCardSmall/BusinessCardSmall.jsx
+++ src/Components/BusinessCardSmall/BusinessCardSmall.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-// import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
