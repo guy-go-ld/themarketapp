f59866da16304c223afaec32a93a42ddab46dbef
talmich <talmichaeli77@gmail.com>
2023-05-23 16:07:37 +0300

    started design profile page in the notification route

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+מimport { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
--- src/routes/notifications/Notifications.component.jsx
+++ src/routes/notifications/Notifications.component.jsx
@@ -1,14 +1,54 @@
 import './Notifications.styles.css';
 import {Component} from "react";
 import {Typography} from "@mui/material";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import ProfileTabsBarComponent from "../../Components/profile-tabs-bar/profile-tabs-bar.component";
 
 class Notifications extends Component{
     render() {
         return(
-            <div style={{paddingTop: "3.5rem"}}>
-                <Typography variant="h2">
-                    Notifications
-                </Typography>
+            <div style={{paddingTop: "5.0rem"}}>
+                <Box sx={{
+                    paddingRight: '1rem',
+                    paddingLeft: '1rem',
+                    paddingTop: '6rem',
+                    paddingBottom: '8rem',
+                    backgroundColor: 'primary.light',
+                    position: 'relative',
+                    borderBottom: 8,
+                    borderBottomColor: 'secondary.main'
+
+                }}>
+                    <Box sx={{
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        display: 'flex',
+                        marginTop: '-50px',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}>
+                        <Box sx={{flexDirection: 'column', margin: 'auto'}}>
+                            <Avatar sx={{
+                                color: "primary.main",
+                                width: 70,
+                                height: 70,
+                                margin: '0 8px',
+                                background: '#ffffff',
+                                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.50)'
+                            }}>
+                                <SupervisedUserCircleIcon sx={{width: 50, height: 50}}/>
+                            </Avatar>
+                            <Typography variant="h2" color="white" boxShadow="unset">User</Typography>
+                        </Box>
+                    </Box>
+                </Box>
+                <ProfileTabsBarComponent>
+
+                </ProfileTabsBarComponent>
+
 
             </div>
         )
